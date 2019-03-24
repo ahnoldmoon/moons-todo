1c427ebff75ff6732082dc8e87b7d9aa9e3789c6
ahnoldmoon <munnt72@hotmail.com>
2019-03-24 10:30:57 +0900

    Styling the To Do Component part 1 Complete.

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+  Platform,
+  ScrollView
+} from 'react-native';
+import ToDo from "./ToDo";
+
 
 const {height, width} = Dimensions.get("window");
 
 export default class App extends React.Component {
+  state = {
+    newToDo: ""
+  };
   render() {
+    const { newToDo} = this.state;
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
         <Text style={styles.title}>Kawai To Do</Text>
         <View style={styles.card}>
-          <TextInput style={styles.input} placeholder={"New To Do"} />
+          <TextInput
+            style={styles.input}
+            placeholder={"New To Do"}
+            value={newToDo}
+            onChangeText={this._crontollNewToDo}
+            placeholderTextColor={"#999"}
+            returnKeyType={"done"}
+            autoCorrect={false}
+          />
+          <ScrollView contentContainerStyle={styles.toDos}>
+            <ToDo />
+          </ScrollView>
         </View>
       </View>
     );
+  }
+
+  _crontollNewToDo = text =>{
+    this.setState({
+      newToDo: text
+    })
   }
 }
 
@@ -51,5 +83,14 @@ const styles = StyleSheet.create({
     //     elevation: 5
     //   }
     // })
+  },
+  input:{
+    padding: 20,
+    borderBottomColor:"#bbb",
+    borderBottomWidth: 1,
+    fontSize: 25
+  },
+  toDos:{
+    alignItems: "center"
   }
 });
