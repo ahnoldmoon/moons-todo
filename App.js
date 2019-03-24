02682a652daee71c4208697b2d416885a005db12
ahnoldmoon <munnt72@hotmail.com>
2019-03-24 13:53:33 +0900

    Saving To Dos part 1 : Complete

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ import {
 } from 'react-native';
 import { AppLoading } from "expo";
 import ToDo from "./ToDo";
-
+import uuidv1 from "uuid/v1";
 
 const {height, width} = Dimensions.get("window");
 
@@ -42,6 +42,7 @@ export default class App extends React.Component {
             placeholderTextColor={"#999"}
             returnKeyType={"done"}
             autoCorrect={false}
+            onSubmitEditing={this._addToDo}
           />
           <ScrollView contentContainerStyle={styles.toDos}>
             <ToDo text={"Hello I'm a To Do"} />
@@ -57,7 +58,34 @@ export default class App extends React.Component {
     })
   }
   _loadToDos = () => {
-
+    this.setState({
+      loadedToDos: true
+    })
+  };
+  _addToDo = () => {
+    const { newToDo } = this.state;
+    if(newToDo !== ""){
+      this.setState(prevState => {
+        const ID = uuidv1();
+        const newToDoObject = {
+          [ID]:{
+            id: ID,
+            isCompleted: false,
+            text: newToDo,
+            createdAt: Date.now()
+          }
+        };
+        const newState = {
+          ...prevState,
+          newToDo: "",
+          toDos:{
+            ...prevState.toDos,
+            ...newToDoObject
+          }
+        }
+        return { ...newState };
+      })
+    }
   };
 }
 
